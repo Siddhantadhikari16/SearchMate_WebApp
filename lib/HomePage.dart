c764c7e1f816c031192220598d14873645a41e8c
Siddhantadhikari16 <siddhantadhikari16@gmail.com>
2024-03-22 02:26:20 +0530

    SearchMAte_WebApp

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,43 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  void performSearch(String searchQuery) {
+    print('Searching for: $searchQuery');
+    String searchUrl = "https://www.google.com/search?q=$searchQuery";
+    webViewController!.loadUrl(urlRequest: URLRequest(url: WebUri(searchUrl)));
+  }
+
   InAppWebViewController? webViewController;
   PullToRefreshController? refreshController;
-  var initialUrl = "https://www.google.com/";
-  var _url="https://www.google.com/";
+  final initialUrl = Uri.parse("https://www.google.com/");
   late var url;
   double progress = 0;
   var urlController = TextEditingController();
+  var isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    refreshController = PullToRefreshController(
+      onRefresh: () {
+        webViewController!.reload();
+      },
+      settings: PullToRefreshSettings(
+          color: Colors.white, backgroundColor: Colors.black),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.cyan,
+          leading: IconButton(
+              onPressed: () async {
+                if (await webViewController!.canGoBack()) {
+                  webViewController!.goBack();
+                }
+              },
+              icon: const Icon(CupertinoIcons.back),
+              iconSize: 30),
           actions: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                    onPressed: () async {
-                      if (await webViewController!.canGoBack()) {
-                        webViewController!.goBack();
-                      }
-                    },
-                    icon: const Icon(CupertinoIcons.back)),
-                IconButton(
                     onPressed: () {
-                      initialUrl;
-                    }, icon: const Icon(Icons.home_filled)),
+                      var webUri = WebUri(initialUrl.toString());
+                      webViewController!
+                          .loadUrl(urlRequest: URLRequest(url: webUri));
+                    },
+                    icon: const Icon(Icons.home_filled)),
                 Container(
                   decoration: const BoxDecoration(
                       color: Colors.white,
@@ -49,25 +72,35 @@ class _HomePageState extends State<HomePage> {
                       padding: const EdgeInsets.only(left: 10),
                       child: TextField(
                         onSubmitted: (value) {
-                          url = Uri.parse(value);
-                          if (url.scheme.isEmpty) {
-                            url = Uri.parse("${initialUrl}search?q=$value");
+                          setState(() {
+                            if (Uri.parse(value).scheme.isEmpty) {
+                              url = "${initialUrl.toString()}search?q=$value";
+                            } else {
+                              url = value;
+                            }
+                          });
+                          if (url != null) {
+                            var webUri = WebUri(url.toString());
+                            webViewController!
+                                .loadUrl(urlRequest: URLRequest(url: webUri));
                           }
-                          webViewController!
-                              .loadUrl(urlRequest: URLRequest(url: url));
                         },
                         controller: urlController,
                         textAlign: TextAlign.start,
                         decoration: InputDecoration(
                             enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
                             fillColor: Colors.grey,
                             hintText: "Search",
                             suffixIcon: IconButton(
-                              onPressed: () {},
                               icon:
                                   const Icon(CupertinoIcons.search_circle_fill),
+                              onPressed: () {
+                                String searchQuery = urlController.text;
+                                performSearch(searchQuery);
+                              },
                               color: Colors.cyan,
-                              iconSize: 35,
+                              iconSize: 30,
                             )),
                       ),
                     ),
@@ -78,7 +111,7 @@ class _HomePageState extends State<HomePage> {
                     webViewController!.reload();
                   },
                   icon: const Icon(Icons.refresh),
-                )
+                ),
               ],
             ),
           ],
@@ -87,14 +120,36 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           children: [
             Expanded(
-                child: InAppWebView(
-              onWebViewCreated: (controller) => webViewController = controller,
-              initialUrlRequest: URLRequest(url: WebUri(initialUrl)),
+                child: Stack(
+              alignment: Alignment.center,
+              children: [
+                InAppWebView(
+                  onLoadStop: (controller, url) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                    refreshController!.endRefreshing();
+                  },
+                  pullToRefreshController: refreshController,
+                  onWebViewCreated: (controller) =>
+                      webViewController = controller,
+                  initialUrlRequest:
+                      URLRequest(url: WebUri(initialUrl.toString())),
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      isLoading = true;
+                    });
+
+                  },
+                ),
+                Visibility(
+                  visible: isLoading,
+                  child: const CircularProgressIndicator(
+                      color: Colors.white, backgroundColor: Colors.cyanAccent),
+                )
+              ],
             )),
           ],
         ));
   }
-  void _launchURL() async =>
-      await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,5 @@ class SearchMate extends StatelessWidget {
     );
   }
 }
+
+
